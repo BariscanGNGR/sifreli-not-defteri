3c503439eafb0d8abcc003af63ba544c4eeb949f
Barışcan Güngör <brsgng@yandex.com>
2021-03-16 12:49:20 +0300

    Add files via upload

---
+++ notDefteri/olustur.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace notDefteri
+{
+    public partial class olustur : Form
+    {
+        private anaEkran context;
+        private string path;
+        private byte secim;
+        private string eskiDosya;
+        public olustur(anaEkran form,string path,byte neYapilacak,string eskiDosya)//0 olustur 1 isim değiştir
+        {
+            InitializeComponent();
+            this.context = form;
+            this.path = path;
+            this.secim = neYapilacak;
+            this.eskiDosya = eskiDosya;
+            FormBorderStyle = FormBorderStyle.None;
+            TopLevel = false;
+            AutoScroll = true;
+
+            if(secim == 1)
+            {
+                button1.Text = "Değiştir";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string newPath = path + @"\" + textBox1.Text + ".not";
+            if (secim == 0)
+            {
+                File.WriteAllText(newPath, "");            
+            }
+            else
+            {
+                string buffer = File.ReadAllText(eskiDosya);
+                File.WriteAllText(newPath, buffer);
+                File.Delete(eskiDosya);
+            }
+            context.dosyaAc(newPath);
+        }
+    }
+}
---
+++ notDefteri/sifreleme.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace notDefteri
+{
+    class sifreleme
+    {
+        private Kullanicilar k;
+        //private anaEkran context;
+        private RSA rsa;
+        public sifreleme(Kullanicilar k)
+        {
+            //this.context = form;
+            this.k = k;
+            //rsa.ImportParameters(k.kurtarmaKey);
+        }
+
+        public string rsaSifreUret()
+        {
+            //rsa = RSA.Create(2048);
+            //return rsa.ExportParameters(true);
+            RSACryptoServiceProvider dec = new RSACryptoServiceProvider(2048);
+            return dec.ToXmlString(true);
+        }
+
+        public static byte[] ByteDonustur(string deger)
+        {
+            UnicodeEncoding ByteConverter = new UnicodeEncoding();
+            return ByteConverter.GetBytes(deger);
+        }
+        public string sifrele(string mesaj)
+        {
+            string sonuc = "";
+            if(mesaj != "" && mesaj != null)
+            {
+                byte[] byteDizi = ByteDonustur(mesaj);
+                RSACryptoServiceProvider dec = new RSACryptoServiceProvider();
+                //dec.ImportParameters(k.kurtarmaKey);
+                dec.FromXmlString(k.kurtarmaKey);
+                byte[] byteCikti = dec.Encrypt(byteDizi, false);
+                sonuc = Convert.ToBase64String(byteCikti);
+            }
+            return sonuc;
+        }
+
+        public string sifreCoz(string mesaj)
+        {
+            string sonuc = "";
+            if (mesaj != "" && mesaj != null)
+            {
+                RSACryptoServiceProvider dec = new RSACryptoServiceProvider();
+                byte[] byteDizi = Convert.FromBase64String(mesaj);
+                UnicodeEncoding ue = new UnicodeEncoding();
+                //dec.ImportParameters(k.kurtarmaKey);
+                dec.FromXmlString(k.kurtarmaKey);
+                byte[] byteCikti = dec.Decrypt(byteDizi, false);
+                sonuc = ue.GetString(byteCikti);
+            }
+            return sonuc;
+        }
+
+        public static string sha256(string yazi)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(yazi));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
